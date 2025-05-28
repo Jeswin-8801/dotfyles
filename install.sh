50e8338492d5e44bdaf05cfd519440386b40ea24
Jeswin Santosh <jeswin.santosh@outlook.com>
2025-05-28 10:44:27 +0530

    add package to support copy to clipboard in editor
    Can be checked using:
    vim --version | grep clipboard

--- install.sh
+++ install.sh
@@ -39,7 +39,7 @@ sudo apt update
 sudo apt -y upgrade
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
   libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
-  libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl
+  libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl vim-gtk3
 # sudo apt -y autoremove
 
 # fish
